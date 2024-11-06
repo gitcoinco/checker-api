b44a552d526ce9b86f335c1e2e40fc608edbcf69
0xKurt <Kurt@Merbeth.io>
2024-11-06 12:42:39 +0100

    Introduce Controllers

--- src/routes/poolRoutes.ts
+++ src/routes/poolRoutes.ts
@@ -1,9 +1,6 @@
-import { createLogger } from '@/logger';
-import poolService from '@/service/PoolService';
-import { catchError } from '@/utils';
+import { createTestPool, getAllPools } from '@/controllers/poolController';
 import { Router } from 'express';
 
-const logger = createLogger('poolRoutes.ts');
 const router = Router();
 
 /**
@@ -15,22 +12,7 @@ const router = Router();
  *       200:
  *         description: A list of pools
  */
-router.get('/', async (req, res) => {
-  logger.info('Received request to get all pools');
-  const [error, pools] = await catchError(poolService.getAllPools());
-
-  if (error !== undefined) {
-    logger.error(`Error fetching pools: ${error.message}`);
-    res
-      .status(500)
-      .json({ message: 'Failed to fetch pools', error: error.message });
-  }
-
-  logger.info(`Successfully fetched ${pools?.length} pools`);
-  res.json({
-    pools,
-  });
-});
+router.get('/', getAllPools);
 
 /**
  * @swagger
@@ -43,22 +25,6 @@ router.get('/', async (req, res) => {
  *       500:
  *         description: Internal server error
  */
-router.post('/create-test', async (req, res) => {
-  logger.info('Received request to create test pool');
-  const [error, pool] = await catchError(poolService.createTestPool());
-
-  if (error !== undefined) {
-    logger.error(`Error creating pool: ${error.message}`);
-    res
-      .status(500)
-      .json({ message: 'Failed to create pool', error: error.message });
-  }
-
-  logger.info(`Successfully created pool with ID: ${pool?.id}`);
-  res.json({
-    message: 'Pool Created',
-    pool,
-  });
-});
+router.post('/create-test', createTestPool);
 
 export default router;
