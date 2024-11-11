d2ff11b0b8bf2f3856393e0d35556715d851b90b
Aditya Anand M C <aditya.anandmc@gmail.com>
2024-11-11 14:18:28 +0530

    fix postgraphile

--- src/postgraphile.config.ts
+++ src/postgraphile.config.ts
@@ -1,4 +1,6 @@
 import { postgraphile } from 'postgraphile';
+import ConnectionFilterPlugin from 'postgraphile-plugin-connection-filter';
+import PgSimplifyInflectorPlugin from '@graphile-contrib/pg-simplify-inflector';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -13,8 +15,28 @@ export const postgraphileMiddleware = postgraphile(DATABASE_URL, 'public', {
   ignoreRBAC: false,
   showErrorStack: 'json',
   extendedErrors: ['hint', 'detail', 'errcode'],
-  appendPlugins: [require('@graphile-contrib/pg-simplify-inflector')],
+  appendPlugins: [PgSimplifyInflectorPlugin, ConnectionFilterPlugin],
   exportGqlSchemaPath: 'schema.graphql',
   graphiql: true,
+  legacyRelations: 'omit',
+  simpleCollections: 'only',
   enhanceGraphiql: true,
+  graphileBuildOptions: {
+    pgOmitListSuffix: true,
+    pgShortPk: true,
+    connectionFilterRelations: true,
+    connectionFilterUseListInflectors: true,
+    connectionFilterAllowedOperators: [
+      'isNull',
+      'equalTo',
+      'notEqualTo',
+      'lessThan',
+      'lessThanOrEqualTo',
+      'greaterThan',
+      'greaterThanOrEqualTo',
+      'in',
+      'notIn',
+      'contains',
+    ],
+  },
 });
